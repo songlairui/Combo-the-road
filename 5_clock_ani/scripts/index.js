c038628d1a5a0b81debd39d53ebdc68ece7fc4fa
songlairui <cnre@live.cn>
2017-04-14 00:12:58 +0800

    ani 版本的时钟。只显示边框。各种hack

--- 5_clock_ani/scripts/index.js
+++ 5_clock_ani/scripts/index.js
@@ -1,3 +1,28 @@
-(function(){
-  return undefined
-})()
+window.onload = function () {
+  // 页面载入时，标记时间
+  var initTime = new Date()
+  var hours, minutes, seconds, ms
+
+  hours = initTime.getHours()
+  minutes = initTime.getMinutes()
+  seconds = initTime.getSeconds()
+  ms = initTime.getMilliseconds()
+
+  var secondDelay, minuteDelay, hourDelay
+
+  secondDelay = - seconds - ms/1000
+  minuteDelay = - minutes * 60 - seconds
+  hourDelay = - hours * 3600 - minutes * 60 - seconds
+
+  var styleElem = document.head.appendChild(document.createElement("style"));
+
+  styleElem.innerHTML = `.clock .above .circle1::before {
+  animation: spin 30s linear ${secondDelay}s infinite, color 60s step-end ${secondDelay}s infinite;
+}
+.clock .above .circle2::before {
+  animation: spin 1800s linear ${minuteDelay}s infinite, color 3600s step-end ${minuteDelay}s infinite;
+}
+.clock .above .circle3::before {
+  animation: spin 21600s linear ${hourDelay}s infinite, color 43200s step-end ${hourDelay}s infinite;
+}`;
+}
