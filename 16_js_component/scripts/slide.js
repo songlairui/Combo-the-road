bd3c3fe6b83fdcbf6394ca6b58a63b2c4bf5e857
songlairui <cnre@live.cn>
2017-05-08 23:37:04 +0800

    slide JS 组件完成

--- 16_js_component/scripts/slide.js
+++ 16_js_component/scripts/slide.js
@@ -4,8 +4,10 @@ class Slide {
     this.target = document.querySelector(options.el)
     this.views = this.target.querySelector('.view')
     this.slides = this.target.querySelectorAll('.slide')
+    this.timer = null
+    this.interval = options.interval || 1600
     // console.info(this.slides)
-    this.initIndicator().bindEvent()
+    this.initIndicator().bindEvent().setTimer()
   }
 
   initIndicator() {
@@ -25,21 +27,76 @@ class Slide {
     this.target.appendChild(oFram)
     return this
   }
+
   bindEvent() {
-    console.info('bind event')
+    // console.info('bind event')
     var dots = this.target.querySelector('.dots')
     var slides = this.slides
-    console.info(dots,slides)
-    dots.addEventListener('click',function(e){
+    // let timer = this.timer
+    let currentIndex = null
+    let _self = this
+    // console.info(dots, slides)
+    dots.addEventListener('click', function (e) {
       // console.info(e)
-      if(e.target.matches('.dot')){
+      if (e.target.matches('.dot')) {
         // console.info('match')
-        let targetIndex = e.target.dataset.target
+        let target = slides[e.target.dataset.target]
+        currentIndex = e.target.dataset.target
         // console.info(slides[targetIndex])
-        slides[targetIndex].style.bgColor='red'
+        // console.info(slides[targetIndex].offsetLeft)
+        target.parentNode.style.transform = `translateX(-${target.offsetLeft}px)`
       }
     })
+    dots.addEventListener('mouseover', function (e) {
+      // console.info('mouseover', _self.timer)
+      if (e.target.matches('.dot')) {
+        e.target.click()
+        clearTimeout(_self.timer)
+        _self.timer = null
+      }
+    })
+    dots.addEventListener('mouseleave', function (e) {
+      _self.setTimer(currentIndex)
+    })
+
+    this.views.addEventListener('mouseover', function (e) {
+      clearTimeout(_self.timer)
+      _self.timer = null
+    })
+    this.views.addEventListener('mouseleave', function (e) {
+      _self.setTimer(currentIndex)
+    })
+    return this
+  }
+
+  setTimer(index) {
+    let inst = typeof index !== 'undefined'
+    index = index || 0
+    let dots = this.target.querySelectorAll('.dots .dot')
+    // let timer = this.timer
+    let _self = this
+    function auto() {
+      // console.info('递归auto', _self.timer)
+      if (_self.timer) {
+        // console.info('timer 重复，返回')
+        return
+      }
+      // console.info('开始设置timer', _self.timer)
+      _self.timer = setTimeout(function () {
+        // console.info('执行timer', _self.timer, Date())
+        _self.timer = null
+        // console.info(dots, index)
+        index = index >= (dots.length - 1) ? 0 : +index + 1
+        dots[index].click()
+        auto()
+      }, inst ? (inst = false, 300) : _self.interval)
+    }
+    auto()
+    return this
   }
 }
 
-new Slide({ el: '.slides' })
+var vmSlide = new Slide({
+  el: '.slides',
+  interval: 1200
+})
