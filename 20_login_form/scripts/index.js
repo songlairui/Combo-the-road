aed39ede7936333d330c138a3a121413163ce11e
songlairui <cnre@live.cn>
2017-06-01 13:50:00 +0800

    加深HTML结构，令Flex布局在safari上也正常显示。 添加事件委托，为表单被点击增加事件。 更改pushBehind动画Z轴值，使后边的form可以点击到。

--- 20_login_form/scripts/index.js
+++ 20_login_form/scripts/index.js
@@ -4,21 +4,50 @@ document.addEventListener('DOMContentLoaded', function() {
     let btnSignIn = main.querySelector('[data-target="signin"]')
     let btnSignUp = main.querySelector('[data-target="signup"]')
     main.addEventListener('click', function(e) {
+      // 当前激活的状态
+      let current = main.dataset.active === 'signup' ? 'signup' : 'signin'
       if (e.target.matches('[data-target]')) {
         console.info('点击到了某个要切换的按钮')
-        let current = main.dataset.active === 'signup' ? 'signup' : 'signin'
         if (current === e.target.dataset.target) {
           return
         }
-        main.setAttribute('data-active', `${e.target.dataset.target}-end`)
-        setTimeout(function() {
-          main.setAttribute('data-active', e.target.dataset.target)
-        }, 0)
+        switchMain(e.target.dataset.target, main)
       }
+      // 如果登录或注册面板被点击，则切换其到焦点状态
+      let clickedBoard = searchEl('[class^="sign"]', e)
+      if (clickedBoard) {
+        // 获取目标状态并标准化
+        let targetActive = [].filter.call(clickedBoard.classList, v => /^sign.*/.test(v))[0] === 'signup' ? 'signup' : 'signin'
+        if (targetActive !== current) {
+          // 如果不相等，说明点了后边的页面，那么把页面提上来。
+          switchMain(targetActive, main)
+          e.preventDefault()
+        }
+      }
+
     })
   }
 })
 
-function searchEl() {
 
+function switchMain(className, main) {
+  if (!main) return console.info('el 未传入')
+  main.setAttribute('data-active', `${className}-end`)
+  setTimeout(function() {
+    main.setAttribute('data-active', className)
+  }, 0)
+}
+
+function searchEl(selector, e) {
+  let root = e.currentTarget || document.documentElement
+  let el = e.target
+  if (!el || !selector) { return null }
+  while (!el.matches(selector)) {
+    if (el === root) {
+      el = null
+      break
+    }
+    el = el.parentNode
+  }
+  return el
 }
